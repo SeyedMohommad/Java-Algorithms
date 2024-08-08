53d5842279829dc60830d5eebd78e6c53086d336
SeyedMohammadAminAltolie-Dev <a.amin771116@yahoo.com>
2024-08-08 11:58:06 +0330

    Searching algorithms - linear search

---
+++ src/SearchingAlgorithms/LinearSearch.java
@@ -0,0 +1,27 @@
+package SearchingAlgorithms;
+
+public class LinearSearch {
+
+    public static int linearSearch(int[] array, int key) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == key) {
+                return i;
+            }
+        }
+        return -1; // key not found
+    }
+
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+        int key = 30;
+
+        int result = linearSearch(array, key);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array");
+        }
+    }
+}
+
---
+++ src/SearchingAlgorithms/README.md
@@ -0,0 +1,17 @@
+# Searching Algorithms
+
+This directory contains implementations of various searching algorithms in Java.
+
+## Algorithms
+
+- **Linear Search**: A simple search algorithm that checks every element in the list until the target value is found or the list ends.
+
+## Usage
+
+### Linear Search
+
+To run the Linear Search algorithm, compile and run the `LinearSearch.java` file:
+
+```bash
+javac LinearSearch.java
+java LinearSearch
